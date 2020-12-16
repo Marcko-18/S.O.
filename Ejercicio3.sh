b22b8709f1309b4ba734087273f6419a0dac1a0a
Marcko-18 <58963231+Marcko-18@users.noreply.github.com>
2020-12-15 22:11:57 -0300

    Create Ejercicio3.sh

---
+++ Ejercicio3.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+if [ !-n $1 ]; then
+  echo "Ningun parametro ingresado"
+  echo "Por favor ingrese un comando luego del nombre del script"
+  echo "ejercicio3.sh + comando a ejecutar"
+fi
